a08d040a4d99fe845cd624a8d875c690e8cf1008
Satyam <satyamsaurav2001@gmail.com>
2024-11-16 10:24:29 +0530

    added more responsiveness

--- src/components/other/AllTask.jsx
+++ src/components/other/AllTask.jsx
@@ -7,28 +7,33 @@ const AllTask = () => {
 
    
   return (
-    <div className=' p-5 rounded mt-5'>
-        <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2 className='text-lg font-medium w-1/5'>Employee Name</h2>
-            <h3 className='text-lg font-medium w-1/5'>New Task</h3>
-            <h5 className='text-lg font-medium w-1/5'>Active Task</h5>
-            <h5 className='text-lg font-medium w-1/5'>Completed</h5>
-            <h5 className='text-lg font-medium w-1/5'>Failed</h5>
-        </div>
-        <div className=''>
-        {userData.map(function(elem,idx){
-            return <div key={idx} className='border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2 className='text-lg font-medium  w-1/5'>{elem.firstName}</h2>
-            <h3 className='text-lg font-medium w-1/5 text-blue-400'>{elem.taskCounts.newTask}</h3>
-            <h5 className='text-lg font-medium w-1/5 text-yellow-400'>{elem.taskCounts.active}</h5>
-            <h5 className='text-lg font-medium w-1/5 text-white'>{elem.taskCounts.completed}</h5>
-            <h5 className='text-lg font-medium w-1/5 text-red-600'>{elem.taskCounts.failed}</h5>
-        </div>
-        })}
-        </div>
-        
-        
+    <div className="p-5 rounded mt-5">
+    {/* Header Section */}
+    <div className="bg-red-400 mb-2 py-2 px-4 flex flex-wrap lg:flex-nowrap justify-between rounded">
+      <h2 className="text-lg font-medium w-full lg:w-1/5 mb-2 lg:mb-0">Employee Name</h2>
+      <h3 className="text-lg font-medium w-full lg:w-1/5 mb-2 lg:mb-0">New Task</h3>
+      <h5 className="text-lg font-medium w-full lg:w-1/5 mb-2 lg:mb-0">Active Task</h5>
+      <h5 className="text-lg font-medium w-full lg:w-1/5 mb-2 lg:mb-0">Completed</h5>
+      <h5 className="text-lg font-medium w-full lg:w-1/5">Failed</h5>
     </div>
+  
+    {/* Dynamic Data Section */}
+    <div>
+      {userData.map((elem, idx) => (
+        <div
+          key={idx}
+          className="border-2 border-emerald-500 mb-2 py-2 px-4 flex flex-wrap lg:flex-nowrap justify-between rounded"
+        >
+          <h2 className="text-lg font-medium w-full lg:w-1/5 mb-2 lg:mb-0">{elem.firstName}</h2>
+          <h3 className="text-lg font-medium w-full lg:w-1/5 mb-2 lg:mb-0 text-blue-400">{elem.taskCounts.newTask}</h3>
+          <h5 className="text-lg font-medium w-full lg:w-1/5 mb-2 lg:mb-0 text-yellow-400">{elem.taskCounts.active}</h5>
+          <h5 className="text-lg font-medium w-full lg:w-1/5 mb-2 lg:mb-0 text-white">{elem.taskCounts.completed}</h5>
+          <h5 className="text-lg font-medium w-full lg:w-1/5 text-red-600">{elem.taskCounts.failed}</h5>
+        </div>
+      ))}
+    </div>
+  </div>
+  
   )
 }
 
